e44f730f9af64478e7bab5c2054f598aaef2c037
jiwonidaax-x <jgb07012@gmail.com>
2022-10-06 15:39:32 -0400

    add comment Count func

--- ChoiJiwon/js/main.js
+++ ChoiJiwon/js/main.js
@@ -1,21 +1,10 @@
 const commentBtn = document.getElementsByClassName("c-submit");
 const commentArea = document.getElementsByClassName("comment");
+const noOfCommentArea = document.getElementsByClassName("f-numOfcomment");
 const write = document.getElementsByClassName("write");
-let username = window.localStorage.getItem('user');
-const btnarr = [];
-const writearr = [];
-
- 
-//collection to array
-for (let element of commentBtn) {
-  btnarr.push(element);
-}
-
-//collection to array
-for (let element of write) {
-  writearr.push(element);
-}
-
+let username = window.localStorage.getItem("user");
+const btnarr = Array.from(commentBtn);
+const writearr = Array.from(write);
 //button activation
 const comment = () => {
   btnarr.forEach((element, idx) => {
@@ -24,17 +13,33 @@ const comment = () => {
 };
 
 //write comment with writer + content
+//if(comment>2) print "댓글 n개 모두 보기" 
 function writeComment() {
   let writer = username;
   writearr.forEach((element, idx) => {
-    var comment = `<span><b>${writer} </b>${element.value}</span><br>`;
+    let numOfComment = commentArea[idx].getElementsByTagName("span").length;
+    var comment = `<span><b>${writer} </b>${element.value}</span>`;
     if (element.value) {
-      commentArea[idx].innerHTML += comment;
-      element.value = " ";
+      if (numOfComment < 2) {
+        commentArea[idx].innerHTML += comment;
+        element.value = "";
+      } else{
+        let allcomment = Array.from(
+          commentArea[idx].getElementsByTagName("span")
+        );
+        allcomment.forEach((element, idx) => {
+          if (idx < allcomment.length - 1) {
+            element.style.display = "none";
+          }
+        });
+        commentArea[idx].innerHTML += comment;
+        let numComment = `<a href="#">댓글 ${numOfComment + 1}개 모두 보기</a>`;
+        noOfCommentArea[idx].innerHTML = numComment;
+        element.value = "";
+      }
     }
   });
 }
-
 /**
  * recognize if keycode is Enter or not, (if enter)=> the comment going to display.
  * (else) when the user writes, the comment button gonna active
