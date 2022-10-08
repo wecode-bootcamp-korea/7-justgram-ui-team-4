9bf3b47111f01ebc6c5b163159ba551b936516bb
RBGonion <yopi27@naver.com>
2022-10-09 00:56:35 +0900

    댓글 삭제 기능 추가

--- JUSTCODE-JH/justagram/js/main.js
+++ JUSTCODE-JH/justagram/js/main.js
@@ -2,10 +2,19 @@ const $button = document.querySelectorAll(".pidComment_input_button");
 const $input = document.querySelectorAll(".pidComment_input_input");
 const $visit = document.querySelectorAll(".pidComment_visit");
 
+function deleteBtnClick(event) {
+  event.target.parentElement.remove();
+}
+
 function clickBtn(event, content, writer, index) {
   event.preventDefault();
   const div = document.createElement("div");
+  div.classList = "comment";
   div.innerHTML = `<span><b>${writer}</b> ${content}</span>`;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "삭제";
+  deleteBtn.addEventListener("click", deleteBtnClick);
+  div.append(deleteBtn);
   $visit[index].append(div);
   div.scrollIntoView({ behavior: "smooth", block: "center" });
   $input[index].value = "";
